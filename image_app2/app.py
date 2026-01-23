98255814b446c9d33fe58ae714cd60836dc15d7f
Human-Ai-Course <okumura.yu@p.chibakoudai.jp>
2026-01-23 18:54:32 +0900

    Initialize Flask application with main route

---
+++ image_app2/app.py
@@ -0,0 +1,15 @@
+# 必要なライブラリをインポート
+from flask import Flask, render_template
+
+# Flaskを使用する準備
+app = Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+@app.route("/")
+def main():
+    """トップページにアクセスしたときに実行される関数"""
+    return render_template("index.html")
+ 
+if __name__ == '__main__':
+    app.run(port=5004)
