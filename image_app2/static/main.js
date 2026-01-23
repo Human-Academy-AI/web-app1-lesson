553d5d0277b79d2028f974c868cfa4303ebf8c76
Human-Ai-Course <okumura.yu@p.chibakoudai.jp>
2026-01-23 19:20:02 +0900

    Update main.js

--- image_app2/static/main.js
+++ image_app2/static/main.js
@@ -1,9 +1,9 @@
 // バナナの画像を表示する
 const changeBanana = () => {
-  document.getElementById("imagebox").src = "static/banana.png";
+  document.getElementById("imagebox").src = "static/banana.jpg";
 };
 
 // ラズパイの画像を表示する
 const changeRaspi = () => {
-  document.getElementById("imagebox").src = "static/raspi.png";
+  document.getElementById("imagebox").src = "static/apple.jpg";
 };
