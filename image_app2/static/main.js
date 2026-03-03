e5242ce93d9c9a7a8cfd9851fb1f48e2a91cf254
Human-Ai-Course <okumura.yu@p.chibakoudai.jp>
2026-03-03 19:27:13 +0900

    Update comment for changeRaspi function

--- image_app2/static/main.js
+++ image_app2/static/main.js
@@ -3,7 +3,7 @@ const changeBanana = () => {
   document.getElementById("imagebox").src = "static/banana.jpg";
 };
 
-// ラズパイの画像を表示する
+// リンゴの画像を表示する
 const changeRaspi = () => {
   document.getElementById("imagebox").src = "static/apple.jpg";
 };
